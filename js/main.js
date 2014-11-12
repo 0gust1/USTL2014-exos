f2f963d35a42a1bf155b1f7932ffba1bde04e58d
0gust1 <augustin.chassine@gmail.com>
2014-11-13 00:36:19 +0100

    dernière passe

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var app = {};
  *         }
  *     ]
  * }
-*/
+ */
 
 app.model = {};
 app.model.name = "";
@@ -26,84 +26,105 @@ app.model.tasks = [];
 /**
  * Ajoute une tâche à la liste
  */
-app._addTask = function addTask(text){
-    app.model.tasks.push({content:text,done:false});
+app._addTask = function addTask(text) {
+    app.model.tasks.push({
+        content: text,
+        done: false
+    });
 };
 
 /**
  * Passe une tâche à "terminé"
  */
-app._taskDone = function taskDone(index){
+app._taskStateSwitch = function taskStateSwitch(index) {
     if (index > -1) {
-        app.model.tasks[index].done=true;
+        app.model.tasks[index].done = !app.model.tasks[index].done;
+    }
+};
+
+app._taskDone = function taskDone(index) {
+    if (index > -1) {
+        app.model.tasks[index].done = true;
     }
 };
 
 /**
  * Passe une tâche à "non terminé"
  */
-app._taskNotDone = function taskNotDone(index){
+app._taskNotDone = function taskNotDone(index) {
     if (index > -1) {
-        app.model.tasks[index].done=false;
+        app.model.tasks[index].done = false;
     }
 };
 
 /**
  * Supprime une tâche
  */
-app._deleteTask = function deleteTask(index){
+app._deleteTask = function deleteTask(index) {
     if (index > -1) {
         app.model.tasks.splice(index, 1);
     }
 };
 
+app.addTask = function(text) {
 
-$(document).ready(function(){
+    //Model
+    app._addTask(text);
+
+    console.dir(app.model.tasks);
+
+    //DOM
+    var task = $("<li class='task-list__item'></li>");
+
+    var content = $("<div class='item-content'>" + text + "</div>");
+    var chkbx = $("<input type='checkbox' class='done-chkbx'>");
+    var button = $("<button class='suppress-btn'>⤬</button>");
+
+    task.append(content);
+    task.prepend(chkbx);
+    task.append(button);
+
+    $("#todo-app ul").append(task);
+
+    $("#create-task").val("");
+};
+
+app.init = function() {
     //mode strict : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Fonctions_et_portee_des_fonctions/Strict_mode
     "use strict";
 
-    $(".task-list").on("click",".suppress-btn",function(){
+    // Delegation d'évènements : 1 event handler sur .task-list, pour gérer les clicks sur le bouton de suppression
+    $(".task-list").on("click", ".suppress-btn", function() {
         var task = $(this).parent("li");
+
         app._deleteTask(task.index());
+
         task.remove();
         console.dir(app.model.tasks);
     });
 
-    $(".task-list").on("change",".done-chkbx",function(){
+    // Delegation d'évènements : 1 event handler sur .task-list, pour gérer le changement d'état sur les tâches
+    $(".task-list").on("change", ".done-chkbx", function() {
 
-        var isTodo = $(this).is(":checked");
         var task = $(this).parent("li");
+        var index = task.index();
 
-        if(isTodo){
-            app._taskDone(task.index());
-        }else{
-            app._taskNotDone(task.index());
-        }
-
+        app._taskStateSwitch(index);
         task.toggleClass("task-list__item--done");
-        console.dir(app.model.tasks);
 
+        console.dir(app.model.tasks);
     });
 
-    $("#create-task").on("change",function(){
+    $("#create-task").on("change", function() {
+        var text = $(this).val();
 
-        app._addTask($(this).val());
-        console.dir(app.model.tasks);
+        app.addTask(text);
 
-        var task = $("<li class='task-list__item'></li>");
-
-        var content = $("<div class='item-content'>"+$(this).val()+"</div>");
-        var chkbx = $("<input type='checkbox' class='done-chkbx'>");
-        var button = $("<button class='suppress-btn'>⤬</button>");
-
-        task.append(content);
-        task.prepend(chkbx);
-        task.append(button);
-
-        $("#todo-app ul").append(task);
-
-        $("#create-task").val("");
     });
+};
+
+$(document).ready(function() {
+
+    app.init();
 
 });
-
